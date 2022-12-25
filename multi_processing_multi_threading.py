f906240950abda9eef9d7ce2a72ffdb832401368
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2022-12-25 16:21:58 +0800

    Create multi_processing_multi_threading.py

---
+++ multi_processing_multi_threading.py
@@ -0,0 +1,57 @@
+# https://realpython.com/python-gil/
+
+import time
+from threading import Thread
+from multiprocessing import Pool
+
+# single_threaded
+COUNT = 50000000
+
+def countdown(n):
+    while n>0:
+        n -= 1
+
+start = time.time()
+countdown(COUNT)
+end = time.time()
+
+print('Time taken in seconds -', end - start)
+# Time taken in seconds - 2.6868488788604736
+
+# multi_threaded
+COUNT = 50000000
+
+def countdown(n):
+    while n>0:
+        n -= 1
+
+t1 = Thread(target=countdown, args=(COUNT//2,))
+t2 = Thread(target=countdown, args=(COUNT//2,))
+
+start = time.time()
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+end = time.time()
+
+print('Time taken in seconds -', end - start)
+# Time taken in seconds - 3.2503092288970947
+
+# multi_processing
+COUNT = 50000000
+def countdown(n):
+    while n>0:
+        n -= 1
+
+if __name__ == '__main__':
+    pool = Pool(processes=2)
+    start = time.time()
+    r1 = pool.apply_async(countdown, [COUNT//2])
+    r2 = pool.apply_async(countdown, [COUNT//2])
+    pool.close()
+    pool.join()
+    end = time.time()
+    print('Time taken in seconds -', end - start)
+    
+# Time taken in seconds - 1.5893323421478271
